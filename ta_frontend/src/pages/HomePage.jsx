54b63fb60fe6d4e54b2eb2d9a424e08b5d45d0ce
Jeremy Puchta <jeremypuchta@outlook.com>
2019-08-04 15:38:05 +0200

    Add Table to Frontend which visualizes traffic data.

--- ta_frontend/src/pages/HomePage.jsx
+++ ta_frontend/src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Intro from '../components/Intro';
 import TimeSeriesChart from '../components/TimeSeriesChart';
+import DataTable from "../components/DataTable";
 
 const HomePage = () => {
   return (
     <Layout>
       <Intro />
       <TimeSeriesChart />
+      <DataTable />
     </Layout>
   );
 };
